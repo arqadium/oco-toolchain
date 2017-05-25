faaa14ac331d89b221853643f89f3d5df34cc330
Alexander Nicholi <alex@caveoforig.in>
2017-05-25 11:16:02 -0400

    Add copyright notice and inline documentation

--- index.py
+++ index.py
@@ -1,10 +1,31 @@
 #!/usr/bin/env python3
 #
+# This document contains proprietary information of TRINITY SOFTWARE and/or its
+# licenced developers and are protected by national and international copyright
+# laws. They may not be disclosed to third parties or copied or duplicated in
+# any form, in whole or in part, without the prior written consent of
+# Trinity Software, LLC.
+#
 
+
+
+## ============================ F U N C T I O N ============================ ##
+## integer main(string[])
+##
+## TITLE:       Main
+## DESCRIPTION: Application entry point.
+##
+## PARAMETER: list of command-line arguments, starting with the script name.
+##
+## RETURNS: Integer exit code; handed back to the operating system.
 def main(args):
 	print('Hello world!')
 	return 0
 
-if(__name__ == '__main__'):
+
+
+## =========================== B O O T S T R A P =========================== ##
+## Application bootstrapper, guarding against code execution upon import.
+if __name__ == '__main__':
 	from sys import argv, exit
 	exit(main(argv))
